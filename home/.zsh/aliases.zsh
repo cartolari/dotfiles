050d33871d62795fc7be75c8b5711ceb8ebf3543
cartolari <bruno.cartolari@gmail.com>
2015-01-17 20:26:01 -0200

    Add bash aliases for apt-get

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -2,3 +2,14 @@ alias ack='ack-grep'
 alias cls='printf "\033c"'
 alias ccat='pygmentize -g -O style=monokai -f console256 -g'
 alias tmux='TERM=screen-256color-bce tmux'
+
+# Apt
+alias apt-install="sudo apt-get install"
+alias apt-update="sudo apt-get update"
+alias apt-ppa="sudo add-apt-repository"
+alias apt-search="apt-cache search"
+alias apt-show="apt-cache show"
+alias apt-remove="sudo apt-get remove"
+alias apt-purge="sudo apt-get purge"
+alias apt-upgrade="sudo apt-get upgrade"
+alias apt-dist-upgrade="sudo apt-get dist-upgrade"

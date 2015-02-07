2628aa92c6914d629d3bc17d7eed9ae29aebcc8c
cartolari <bruno.cartolari@gmail.com>
2015-02-07 02:38:14 -0200

    Unalias git to gh

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -2,7 +2,6 @@ alias ack='ack-grep'
 alias cls='printf "\033c"'
 alias ccat='pygmentize -g -O style=monokai -f console256 -g'
 alias tmux='TERM=screen-256color-bce tmux'
-alias git='gh'
 alias vim='nvim'
 
 # Apt

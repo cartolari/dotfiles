79b8058e0e6619645feee66d0593d895b89c753d
cartolari <bruno.cartolari@gmail.com>
2015-09-02 23:39:05 -0300

    Pick the appropriate ruby version for rubocop

--- home/rubocop.sh
+++ home/rubocop.sh
@@ -1,8 +1,12 @@
-#! /bin/sh
+#!/bin/bash
+
+source /usr/local/share/chruby/chruby.sh
+chruby 2.1.5
 if [ $# -eq 1 -a x"$1" = x--version ]; then
-    rubocop "$@" 2>&1 | \
-        sed '/warning: parser\/current is loading parser\/ruby[0-9]*, which recognizes/d;
-             /warning: [0-9.]*-compliant syntax, but you are running [0-9.]/d'
+  rubocop "$@" 2>&1 | \
+    sed '/warning: parser\/current is loading parser\/ruby[0-9]*, which recognizes/d;
+         /warning: [0-9.]*-compliant syntax, but you are running [0-9.]/d;
+         /warning: please see https:\/\/github.com\/whitequark\/parser#compatibility-with-ruby-mri/d'
 else
-    exec rubocop "$@"
+  exec rubocop "$@"
 fi

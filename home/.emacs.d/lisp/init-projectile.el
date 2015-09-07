515e7af6c532dce726c4971d39f89fbe49ede290
cartolari <bruno.cartolari@gmail.com>
2015-09-07 16:42:31 -0300

    Use helm as the projectile completion system

--- home/.emacs.d/lisp/init-projectile.el
+++ home/.emacs.d/lisp/init-projectile.el
@@ -10,6 +10,9 @@
 
 (projectile-global-mode 1)
 
+(setq projectile-completion-system 'helm
+      projectile-switch-project-action 'helm-projectile)
+
 (diminish 'projectile-mode " ℗ ")
 
 (provide 'init-projectile)

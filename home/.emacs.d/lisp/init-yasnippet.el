20c1036c8c4fd632297693ee40f2b708b0bcce51
cartolari <bruno.cartolari@gmail.com>
2015-09-20 15:48:41 -0300

    Complete snippets with company mode

--- home/.emacs.d/lisp/init-yasnippet.el
+++ home/.emacs.d/lisp/init-yasnippet.el
@@ -11,6 +11,51 @@
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets" "~/code/yasnippet-snippets"))
 
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or
+       (not company-mode/enable-yas)
+       (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+
+(setq company-backends
+      (mapcar #'company-mode/backend-with-yas company-backends))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "->") t nil)))))
+
+(defun do-yas-expand ()
+  "Expand a YAS snippet."
+  (let ((yas-fallback-behavior 'return-nil))
+    (yas-expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
+(global-set-key (kbd "<tab>") 'tab-indent-or-complete)
+(define-key company-active-map [tab] 'company-select-next)
+(define-key company-active-map [backtab] 'company-select-previous)
+(define-key company-active-map (kbd "<tab>") 'company-select-next)
+(define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+
 (yas-global-mode 1)
 
 (diminish 'yas-minor-mode)

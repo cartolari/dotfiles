79f2b6ec115416dc751e01a86571574e6cc0c5ad
cartolari <bruno.cartolari@gmail.com>
2015-09-21 01:22:32 -0300

    Removing all unused placeholders from powerline

--- home/.emacs.d/lisp/init-powerline.el
+++ home/.emacs.d/lisp/init-powerline.el
@@ -65,94 +65,89 @@
 (defun powerline-spacemacs-imitation-theme ()
   "An attempt to imitate the spacemacs powerline theme."
   (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-                          (face2 (if active 'powerline-active2 'powerline-inactive2))
-                          (face3 (if active 'powerline-custom1 mode-line))
-                          (face4 (if active 'powerline-custom2 mode-line))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (powerline-raw "%*" face3 'l)
-                                     (powerline-raw " " face3)
-                                     (powerline-raw (powerline-evil-state) face3)
-                                     (funcall separator-left face3 mode-line)
+  (setq-default
+   mode-line-format
+   '("%e"
+     (:eval
+      (let* ((active (powerline-selected-window-active))
+             (mode-line (if active 'mode-line 'mode-line-inactive))
+             (face1 (if active 'powerline-active1 'powerline-inactive1))
+             (face2 (if active 'powerline-active2 'powerline-inactive2))
+             (face3 (if active 'powerline-custom1 mode-line))
+             (face4 (if active 'powerline-custom2 mode-line))
+             (separator-left (intern (format "powerline-%s-%s"
+                                             (powerline-current-separator)
+                                             (car powerline-default-separator-dir))))
+             (separator-right (intern (format "powerline-%s-%s"
+                                              (powerline-current-separator)
+                                              (cdr powerline-default-separator-dir))))
+             (lhs (list (powerline-raw "%*" face3 'l)
+                        (powerline-raw " " face3)
+                        (powerline-raw (powerline-evil-state) face3)
+                        (funcall separator-left face3 mode-line)
 
-                                     (when powerline-display-buffer-size
-                                       (powerline-buffer-size nil 'l))
-                                     (when powerline-display-mule-info
-                                       (powerline-raw mode-line-mule-info face4 'l))
-                                     (powerline-buffer-id face4 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format nil 'l))
-                                     (powerline-raw " ")
-                                     (funcall separator-right mode-line face1)
+                        (when powerline-display-buffer-size
+                          (powerline-buffer-size nil 'l))
+                        (when powerline-display-mule-info
+                          (powerline-raw mode-line-mule-info face4 'l))
+                        (powerline-buffer-id face4 'l)
+                        (when (and (boundp 'which-func-mode) which-func-mode)
+                          (powerline-raw which-func-format nil 'l))
+                        (powerline-raw " ")
+                        (funcall separator-right mode-line face1)
 
-                                     (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object face1 'l))
-                                     (powerline-major-mode face1 'l)
-                                     (powerline-process face1)
-                                     (powerline-raw " " face1)
-                                     (funcall separator-left face1 mode-line)
+                        (when (boundp 'erc-modified-channels-object)
+                          (powerline-raw erc-modified-channels-object face1 'l))
+                        (powerline-major-mode face1 'l)
+                        (powerline-process face1)
+                        (powerline-raw " " face1)
+                        (funcall separator-left face1 mode-line)
 
-                                     ;; (powerline-raw " FLYCHECK " 'powerline-flycheck-error 'l)
-                                     ;; FLYCHECK start
-                                     (powerline-raw (format " • %d" (powerline-flycheck-error-count))
-                                                    'powerline-flycheck-error)
-                                     (powerline-raw (format " • %d" (powerline-flycheck-warning-count))
-                                                    'powerline-flycheck-warning)
-                                     (powerline-raw (format " • %d" (powerline-flycheck-info-count))
-                                                    'powerline-flycheck-info)
+                        ;; FLYCHECK start
+                        (powerline-raw (format " • %d" (powerline-flycheck-error-count))
+                                       'powerline-flycheck-error)
+                        (powerline-raw (format " • %d" (powerline-flycheck-warning-count))
+                                       'powerline-flycheck-warning)
+                        (powerline-raw (format " • %d" (powerline-flycheck-info-count))
+                                       'powerline-flycheck-info)
 
-                                     (powerline-raw " " mode-line)
+                        (powerline-raw " " mode-line)
 
-                                     (funcall separator-right mode-line face1)
-                                     (powerline-raw " " face1)
-                                     (powerline-raw " EMPTY 1 " face1)
-                                     (powerline-raw " " face1)
-                                     ;; FLYCHECK end
+                        (funcall separator-right mode-line face1)
+                        ;; FLYCHECK end
 
-                                     (funcall separator-left face1 mode-line)
+                        (powerline-minor-modes face1 'l)
+                        (powerline-raw " " face1)
+                        (funcall separator-left face1 mode-line)
 
-                                     (powerline-minor-modes mode-line 'l)
-                                     (powerline-raw " " mode-line)
-                                     (funcall separator-right mode-line face1)
-                                     (powerline-raw " EMPTY 2 " face1)
-                                     (funcall separator-left face1 mode-line)
-
-                                     (powerline-raw " EMPTY ENTRY " mode-line 'l)
-                                     (powerline-raw " " mode-line)
-                                     (funcall separator-right mode-line face1)
-
-                                     (powerline-vc face1 'r)
-                                     (powerline-raw " " face1)
-                                     (funcall separator-left face1 face2)))
-                          (rhs (list (powerline-raw global-mode-string face2 'r)
-                                     (funcall separator-right face2 face1)
-                                     (unless window-system
-                                       (powerline-raw (char-to-string #xe0a1) face1 'l))
-                                     (powerline-raw "%4l" face1 'l)
-                                     (powerline-raw ":" face1 'l)
-                                     (powerline-raw "%3c" face1 'r)
-                                     (funcall separator-right face1 mode-line)
-                                     (powerline-raw " ")
-                                     (powerline-raw "%6p" nil 'r))))
-                     (concat (powerline-render lhs)
-                             (powerline-fill face2 (powerline-width rhs))
-                             (powerline-render rhs)))))))
+                        (powerline-vc mode-line 'r)
+                        (powerline-raw " " mode-line)
+                        (funcall separator-right mode-line face2)))
+             (rhs (list (powerline-raw global-mode-string face2 'r)
+                        (funcall separator-right face2 face1)
+                        (powerline-raw "%4l" face1 'l)
+                        (powerline-raw ":" face1 'l)
+                        (powerline-raw "%3c" face1 'r)
+                        (funcall separator-right face1 mode-line)
+                        (powerline-raw " ")
+                        (powerline-raw "%6p" nil 'r))))
+        (concat (powerline-render lhs)
+                (powerline-fill face2 (powerline-width rhs))
+                (powerline-render rhs)))))))
 
 (defun set-mode-line-font ()
   "Update mode-line font.
 Needs investigation on why it's needed to setup as an
 hook otherwise the settings didn't get applied"
   (set-face-attribute 'mode-line nil :font "Source Code Pro for Powerline-10"))
+
+;; Remove the Git string from the vc-mode-line
+(defadvice vc-mode-line (after strip-backend () activate)
+  (when (stringp vc-mode)
+    (let ((noback (replace-regexp-in-string
+                   (format "^ %s" (vc-backend buffer-file-name))
+                   " " vc-mode)))
+      (setq vc-mode noback))))
 
 (powerline-spacemacs-imitation-theme)
 (setq powerline-default-separator 'wave)

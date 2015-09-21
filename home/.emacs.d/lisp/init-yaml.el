be83e9f0149408620150811e4d48044ed060b4cf
cartolari <bruno.cartolari@gmail.com>
2015-09-20 21:17:20 -0300

    Use company-dabbrev-code-modes in yaml

--- home/.emacs.d/lisp/init-yaml.el
+++ home/.emacs.d/lisp/init-yaml.el
@@ -11,6 +11,7 @@
 (add-hook 'yaml-mode-hook (lambda () (setq evil-shift-width yaml-indent-offset)))
 (add-hook 'yaml-mode-hook (lambda () (setq outline-regexp " *")))
 (add-hook 'yaml-mode-hook 'outline-minor-mode)
+(add-to-list 'company-dabbrev-code-modes 'yaml-mode)
 
 (provide 'init-yaml)
 

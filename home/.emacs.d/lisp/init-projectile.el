10de04c880c9e0c04a8f3eed9000dccc5f022724
cartolari <bruno.cartolari@gmail.com>
2015-10-23 12:07:02 -0200

    Change ,t binding to 'helm-projectile

--- home/.emacs.d/lisp/init-projectile.el
+++ home/.emacs.d/lisp/init-projectile.el
@@ -8,7 +8,7 @@
   :config
   (projectile-global-mode 1)
   (define-key projectile-mode-map (kbd "C-c p R") 'my/projectile-regenerate-tags)
-  (key-seq-define evil-normal-state-map ",t" 'projectile-swith-project-or-find-file)
+  (key-seq-define evil-normal-state-map ",t" 'helm-projectile)
   :diminish projectile-mode)
 
 (defun my/projectile-regenerate-tags ()
@@ -19,13 +19,6 @@
      "tags" nil (format "cd %s; git ctags"
                         (projectile-project-root)))
     (message "Generating tags file")))
-
-(defun projectile-swith-project-or-find-file ()
-  "Find a file in current (if any) or all projectile projects."
-  (interactive)
-  (if (projectile-project-p)
-      (helm-projectile-find-file)
-    (helm-projectile-switch-project)))
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here

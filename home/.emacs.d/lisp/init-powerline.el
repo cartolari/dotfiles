53f1a8d44f8b74bc4befc1f6dc7b546922c1c8cc
cartolari <bruno.cartolari@gmail.com>
2015-10-23 00:07:09 -0200

    Use eyebrowse in Emacs

--- home/.emacs.d/lisp/init-powerline.el
+++ home/.emacs.d/lisp/init-powerline.el
@@ -4,21 +4,45 @@
 ;;; Try to mimic Spacemacs modeline
 
 ;;; Code:
+(use-package eyebrowse
+  :config
+  (eyebrowse-mode)
+  (setq eyebrowse-new-workspace t)
+  (bind-key "s-w" (defhydra hydra-eyebrowse ()
+                    "eyebrowse"
+                    ("n" eyebrowse-next-window-config "next")
+                    ("N" (lambda ()
+                           (interactive)
+                           (eyebrowse-switch-to-window-config
+                            (+ 1 (eyebrowse--get 'current-slot))))
+                     "new")
+                    ("p" eyebrowse-prev-window-config "previous")
+                    ("c" eyebrowse-close-window-config "close")
+                    ("l" eyebrowse-last-window-config "last")
+                    ("q" nil "quit"))
+            eyebrowse-mode-map)
+  :diminish eyebrowse-mode)
+
+(use-package window-numbering
+  :config (window-numbering-mode))
+
 (use-package spaceline
   :config
   (require 'spaceline-config)
   (spaceline-spacemacs-theme)
-  (setq powerline-default-separator 'wave)
-  (setq powerline-height 25)
   (setq anzu-cons-mode-line-p nil)
   (setq anzu-mode-line-update-function
         (lambda (here total)
           (propertize (format "<%d/%d>" here total)
                       'face (spaceline-highlight-face-default))))
+  (setq powerline-default-separator 'wave)
+  (setq powerline-height 25)
+  (setq spaceline-window-numbers-unicode t)
+  (setq spaceline-workspace-numbers-unicode t)
   (set-face-attribute 'mode-line nil :font "Source Code Pro for Powerline-10")
   (set-face-attribute (spaceline-highlight-face-default) nil :weight 'bold)
   (spaceline-install
-   '(((anzu evil-state) :face highlight-face)
+   '(((anzu workspace-number window-number evil-state) :face highlight-face :separator " | ")
      (buffer-modified buffer-size buffer-id remote-host)
      major-mode
      ((flycheck-error flycheck-warning flycheck-info) :when active)

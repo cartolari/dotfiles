f0b8d59e3819d7e0f14a01c6cc97d126b3513c98
cartolari <bruno.cartolari@gmail.com>
2016-02-13 16:43:55 -0200

    Replace some functions by crux package

--- home/.emacs.d/lisp/init-general-editing.el
+++ home/.emacs.d/lisp/init-general-editing.el
@@ -18,6 +18,8 @@
   :init
   (add-hook 'after-init-hook 'global-auto-revert-mode))
 
+(use-package crux)
+
 (use-package discover-my-major
   :commands (discover-my-major discover-my-mode))
 
@@ -78,31 +80,6 @@
   :config
   (setq-default save-place t)
   :ensure nil)
-
-(defun rename-file-and-buffer ()
-  "Rename the current buffer and file it is visiting."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (message "Buffer is not visiting a file!")
-      (let ((new-name (read-file-name "New name: " filename)))
-        (cond
-         ((vc-backend filename) (vc-rename-file filename new-name))
-         (t
-          (rename-file filename new-name t)
-          (set-visited-file-name new-name t t)))))))
-
-(defun delete-file-and-buffer ()
-  "Kill the current buffer and deletes the file it is visiting."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (when filename
-      (if (vc-backend filename)
-          (vc-delete-file filename)
-        (progn
-          (delete-file filename)
-          (message "Deleted file %s" filename)
-          (kill-buffer))))))
 
 (defun sudo-save ()
   "Save a file using sudo."

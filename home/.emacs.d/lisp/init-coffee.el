994ff45fb778b4388c4b6cf9f7a6e189920ae01a
cartolari <bruno.cartolari@gmail.com>
2016-02-14 16:09:16 -0200

    Fix coffee-mode indentation in evil-mode

--- home/.emacs.d/lisp/init-coffee.el
+++ home/.emacs.d/lisp/init-coffee.el
@@ -6,7 +6,24 @@
 ;;; Code:
 (require 'use-package)
 
+(defun my/coffee-evil-previousline-and-indent ()
+  "Fix 'coffee-newline-and-indent' with 'evil-mode'."
+  (interactive)
+  (forward-line -1)
+  (my/coffee-evil-newline-and-indent))
+
+(defun my/coffee-evil-newline-and-indent ()
+  "Fix 'coffee-newline-and-indent' with 'evil-mode'."
+  (interactive)
+  (end-of-line)
+  (coffee-newline-and-indent)
+  (evil-insert-state))
+
 (use-package coffee-mode
+  :config
+  (evil-define-key 'normal coffee-mode-map "o" 'my/coffee-evil-newline-and-indent)
+  (evil-define-key 'normal coffee-mode-map "O" 'my/coffee-evil-previousline-and-indent)
+  (evil-define-key 'insert coffee-mode-map "RET" 'my/coffee-evil-newline-and-indent)
   :mode ("\\.coffee\\'" . coffee-mode))
 
 (provide 'init-coffee)

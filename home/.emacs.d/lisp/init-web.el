643ee9abcafa48bd89c615adf02cbe91d7bd99eb
cartolari <bruno.cartolari@gmail.com>
2016-02-21 23:12:46 -0300

    Add web-mode to Emacs

--- home/.emacs.d/lisp/init-web.el
+++ home/.emacs.d/lisp/init-web.el
@@ -12,14 +12,27 @@
   :init
   (add-hook 'css-mode-hook  'emmet-mode)
   (add-hook 'sgml-mode-hook 'emmet-mode))
+
 (use-package rainbow-mode
   :commands (rainbow-mode)
   :diminish rainbow-mode
   :init
   (add-hook 'css-mode-hook 'rainbow-mode)
   (add-hook 'scss-mode-hook 'rainbow-mode))
+
 (use-package scss-mode
   :mode ("\\.scss\\'" . scss-mode))
+
+(use-package web-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
 
 (setq-default css-indent-offset 2)
 

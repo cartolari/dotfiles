d02154efab16a23ee2989990e649c91e30c7e9a6
cartolari <bruno.cartolari@gmail.com>
2016-02-28 17:56:34 -0300

    Add company-shell to Emacs

--- home/.emacs.d/lisp/init-sh.el
+++ home/.emacs.d/lisp/init-sh.el
@@ -6,13 +6,21 @@
 ;;; Code:
 (require 'use-package)
 
-(setq-default sh-basic-offset 2)
-(setq-default sh-indentation 2)
-(add-hook 'sh-mode-hook
-          (lambda()
-            (setq evil-shift-width sh-indentation)))
-(add-hook 'after-save-hook
-          'executable-make-buffer-file-executable-if-script-p)
+(use-package sh-script
+  :config
+  (setq-default sh-basic-offset 2
+                sh-indentation 2)
+  (add-hook 'sh-mode-hook
+            (lambda()
+              (setq evil-shift-width sh-indentation)))
+  (add-hook 'after-save-hook
+            'executable-make-buffer-file-executable-if-script-p)
+  :ensure nil)
+
+(use-package company-shell
+  :commands (company-shell-mode)
+  :init
+  (add-hook 'sh-mode-hook 'company-shell-mode))
 
 (provide 'init-sh)
 ;;; init-sh.el ends here

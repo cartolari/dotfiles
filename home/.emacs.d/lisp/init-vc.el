5f698d4030137c8e8b85333529b3ea9818a56426
cartolari <bruno.cartolari@gmail.com>
2016-02-28 13:37:04 -0300

    Add diff-hl mode to Emacs

--- home/.emacs.d/lisp/init-vc.el
+++ home/.emacs.d/lisp/init-vc.el
@@ -6,11 +6,38 @@
 ;;; Code:
 (require 'use-package)
 
+(use-package diff-hl
+  :commands (diff-hl-dired-mode
+             diff-hl-margin-mode
+             diff-hl-amend-mode
+             diff-hl-flydiff-mode)
+  :config
+  (set-face-attribute 'diff-hl-change nil
+                      :foreground "blue3"
+                      :background "#5656ff")
+  (set-face-attribute 'diff-hl-delete nil
+                      :foreground "red"
+                      :background "red")
+  (set-face-attribute 'diff-hl-insert nil
+                      :foreground "#098d07"
+                      :background "#ddffdf")
+  (setq diff-hl-side 'right)
+  :init
+  (add-hook 'prog-mode-hook 'diff-hl-mode)
+  (add-hook 'prog-mode-hook 'diff-hl-flydiff-mode)
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+  (add-hook 'dired-mode-hook 'diff-hl-flydiff-mode)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+
 (use-package git-timemachine
   :commands (git-timemachine))
+
 (use-package gitattributes-mode)
+
 (use-package gitconfig-mode)
+
 (use-package gitignore-mode)
+
 (use-package magit
   :bind ("C-x g" . magit-status))
 

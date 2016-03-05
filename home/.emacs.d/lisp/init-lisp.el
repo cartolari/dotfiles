aa01eadd9ad0dae8f286619905119ed63f7e3555
cartolari <bruno.cartolari@gmail.com>
2016-03-05 15:56:42 -0300

    Add eval-and-replace to Elisp mode

--- home/.emacs.d/lisp/init-lisp.el
+++ home/.emacs.d/lisp/init-lisp.el
@@ -23,7 +23,20 @@
   (add-hook 'eval-expression-minibuffer-setup-hook 'paredit-mode)
   :diminish paredit-mode)
 
-(add-hook 'emacs-lisp-mode-hook (lambda() (setq mode-name "Elisp")))
+(use-package elisp-mode
+  :ensure nil
+  :config
+  (defun eval-and-replace ()
+    "Replace the preceding sexp with its value."
+    (interactive)
+    (backward-kill-sexp)
+    (condition-case nil
+        (prin1 (eval (read (current-kill 0)))
+               (current-buffer))
+      (error (message "Invalid expression")
+             (insert (current-kill 0)))))
+  (add-hook 'emacs-lisp-mode-hook (lambda() (setq mode-name "Elisp")))
+  (define-key emacs-lisp-mode-map (kbd "C-c C-e") 'eval-and-replace))
 
 (provide 'init-lisp)
 ;;; init-lisp.el ends here

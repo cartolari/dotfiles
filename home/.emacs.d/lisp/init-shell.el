7aa0ad592d93088391e707120a4b650142377d64
cartolari <bruno.cartolari@gmail.com>
2016-09-10 20:19:43 -0300

    Add some tweaks to eshell

--- home/.emacs.d/lisp/init-shell.el
+++ home/.emacs.d/lisp/init-shell.el
@@ -24,5 +24,23 @@
   (bind-keys :map shell-mode-map ("C-C C-l" . helm-comint-input-ring))
   :ensure nil)
 
+(use-package eshell-fringe-status
+  :commands (eshell-fringe-status-mode)
+  :init
+  (add-hook 'eshell-mode-hook 'eshell-fringe-status-mode))
+
+(use-package eshell-did-you-mean
+  :commands (eshell-did-you-mean-setup)
+  :init
+  (add-hook 'eshell-mode-hook 'eshell-did-you-mean-setup))
+
+
+(use-package eshell-prompt-extras
+  :init
+  (with-eval-after-load "esh-opt"
+    (autoload 'epe-theme-lambda "eshell-prompt-extras")
+    (setq eshell-highlight-prompt nil
+          eshell-prompt-function 'epe-theme-lambda)))
+
 (provide 'init-shell)
 ;;; init-shell.el ends here

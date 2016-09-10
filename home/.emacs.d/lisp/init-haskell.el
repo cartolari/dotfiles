5bc65cb0d5f971a5048c02c7a429be5e99b56ede
cartolari <bruno.cartolari@gmail.com>
2016-09-10 20:21:40 -0300

    Add haskell-mode to Emacs

---
+++ home/.emacs.d/lisp/init-haskell.el
@@ -0,0 +1,11 @@
+;;; init-haskell.el -- Haskell language support
+
+;;; Commentary:
+;;; Haskell language support
+
+;;; Code:
+(use-package  haskell-mode
+  :mode ("\\.hs\\'" . gfm-mode))
+
+(provide 'init-haskell)
+;;; init-haskell.el ends here

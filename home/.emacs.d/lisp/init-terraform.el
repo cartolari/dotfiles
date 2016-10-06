89e6ccfbfc301e4b8840bebd59f70b8302c54708
cartolari <bruno.cartolari@gmail.com>
2016-10-06 14:04:19 -0300

    Format Terraform files on save

--- home/.emacs.d/lisp/init-terraform.el
+++ home/.emacs.d/lisp/init-terraform.el
@@ -6,7 +6,9 @@
 ;;; Code:
 (use-package terraform-mode
   :mode (("\\.tf\\'" . terraform-mode)
-         ("\\.tfvars\\'" . terraform-mode)))
+         ("\\.tfvars\\'" . terraform-mode))
+  :config
+  (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode))
 
 (provide 'init-terraform)
 ;;; init-terraform.el ends here

61e71c8a9e909da06155152b0f4ca0a67262a00c
cartolari <bruno.cartolari@gmail.com>
2017-08-20 23:08:57 -0300

    Refactor pacaur_install function

--- install-packages.sh
+++ install-packages.sh
@@ -111,8 +111,8 @@ pacaur_install () {
   package=$1
   echo "Installing $package"
   set +e
-  pacaur -Qs "^$package$" > /dev/null
-  if [[ $? -eq 0 ]]; then
+
+  if pacaur -Qs "^$package$" > /dev/null; then
     return
   fi
   set -e

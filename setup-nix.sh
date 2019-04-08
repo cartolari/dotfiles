1f6ba69613a15e156bd8b140fd81c964596c2869
cartolari <bruno.cartolari@gmail.com>
2019-04-07 22:58:21 -0300

    Add homesick

--- setup-nix.sh
+++ setup-nix.sh
@@ -36,6 +36,7 @@ nix-env -f "<nixpkgs>" -i -A curl
 nix-env -f "<nixpkgs>" -i -A dnsmasq
 nix-env -f "<nixpkgs>" -i -A git
 nix-env -f "<nixpkgs>" -i -A gptfdisk
+nix-env -f "<nixpkgs>" -i -A homesick
 nix-env -f "<nixpkgs>" -i -A htop
 nix-env -f "<nixpkgs>" -i -A inetutils
 nix-env -f "<nixpkgs>" -i -A less

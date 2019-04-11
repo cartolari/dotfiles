29a6abb1eca25f7cdbde51e595f14c705f373bc5
cartolari <bruno.cartolari@gmail.com>
2019-04-11 14:55:08 -0300

    Fix package priorities

--- setup-nix.sh
+++ setup-nix.sh
@@ -37,7 +37,7 @@ nix-env -f "<nixpkgs>" -i -A dnsmasq
 nix-env -f "<nixpkgs>" -i -A git
 nix-env -f "<nixpkgs>" -i -A gnused
 nix-env -f "<nixpkgs>" -i -A gptfdisk
-nix-env -f "<nixpkgs>" -i -A homesick; nix-env --set-flag priority 10 homesick; nix-env --set-flag priority 3 ruby
+nix-env -f "<nixpkgs>" -i -A homesick; nix-env --set-flag priority 15 homesick
 nix-env -f "<nixpkgs>" -i -A htop
 nix-env -f "<nixpkgs>" -i -A inetutils
 nix-env -f "<nixpkgs>" -i -A less
@@ -45,7 +45,7 @@ nix-env -f "<nixpkgs>" -i -A libguestfs
 nix-env -f "<nixpkgs>" -i -A lxc
 nix-env -f "<nixpkgs>" -i -A lxd
 nix-env -f "<nixpkgs>" -i -A man-pages
-nix-env -f "<nixpkgs>" -i -A man; nix-env --set-flag priority 4 man
+nix-env -f "<nixpkgs>" -i -A man
 nix-env -f "<nixpkgs>" -i -A nodejs-11_x
 nix-env -f "<nixpkgs>" -i -A openssh
 nix-env -f "<nixpkgs>" -i -A qemu

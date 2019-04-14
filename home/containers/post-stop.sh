781f21b4c2ded7ccab748b6f28630676cc06c830
cartolari <bruno.cartolari@gmail.com>
2019-04-14 02:35:02 -0300

    Remove unused LXC hooks

--- home/containers/post-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-if mountpoint "$LXC_ROOTFS_PATH"; then
-  umount "$LXC_ROOTFS_PATH"
-fi
-
-[[ -b /dev/nbd0p1 ]] && qemu-nbd --diconnect /dev/nbd0

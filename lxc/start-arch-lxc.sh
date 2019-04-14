6e768ac1beee61d8ace4dfdebf368f6534210817
cartolari <bruno.cartolari@gmail.com>
2019-04-14 03:36:32 -0300

    Change LXC config to run with Nix provided LXC

--- lxc/start-arch-lxc.sh
+++ lxc/start-arch-lxc.sh
@@ -5,6 +5,9 @@ IFS=$'\n\t'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CONTAINERS=/home/chronos/user/containers
+LXC_PREFIX=/home/chronos/user/.nix-profile
+
+export PATH=$LXC_PREFIX/bin:$PATH
 
 if [[ $EUID -ne 0 ]]; then
  echo "This script must be run as root" 1>&2
@@ -24,37 +27,40 @@ prompt_confirmation() {
 }
 
 # Network
-if ! grep -E '^USE_LXC_BRIDGE="true"' /usr/local/etc/default/lxc > /dev/null; then
-  echo Enabling LXC bridge
-  sed -i 's/USE_LXC_BRIDGE="false"/USE_LXC_BRIDGE="true"/' /usr/local/etc/default/lxc
-  cat /usr/local/etc/default/lxc
+if [[ ! -f /etc/default/lxc-net ]]; then
+  echo Creating /etc/default/lxc-net
+  cat <<EOF > /etc/default/lxc-net
+USE_LXC_BRIDGE="true"
+LXC_BRIDGE="lxcbr0"
+LXC_ADDR="10.0.3.1"
+LXC_NETMASK="255.255.255.0"
+LXC_NETWORK="10.0.3.0/24"
+EOF
 else
-  echo LXC bridge is enabled
+  echo Using existing /etc/default/lxc-net
 fi
 
-if grep CHECKSUM /usr/local/libexec/lxc/lxc-net; then
+if grep CHECKSUM $LXC_PREFIX/libexec/lxc/lxc-net; then
   # There is no easy way to get iptables extensions in ChromiumOS
   echo 'Removing CHECKSUM from iptables rules'
-  sed -i '/-j CHECKSUM/d' /usr/local/libexec/lxc/lxc-net
+  sed -i '/-j CHECKSUM/d' $LXC_PREFIX/libexec/lxc/lxc-net
 fi
 
-[[ -d /usr/local/var/lib/misc/ ]] || mkdir -p /usr/local/var/lib/misc/
-
 if ! ip a | grep lxcbr0 > /dev/null; then
-  /usr/local/libexec/lxc/lxc-net start
+  $LXC_PREFIX/libexec/lxc/lxc-net start
 fi
 
 # Storage
-[[ -d /home/chronos/user/containers ]] || mkdir -p /home/chronos/user/containers
+mkdir -p "$CONTAINERS"
 
 if [[ ! -f "$CONTAINERS/arch.img" ]]; then
   prompt_confirmation "File $CONTAINERS/arch.img does not exist. Create it?"
 
   for i in seq 1 10; do
     time dd if=/dev/zero bs=1M count=5000 | pv -s 5G -L 150M >> "$CONTAINERS/arch.img"
-    sleep 2
+    sleep 10
     time sync
-    sleep 15
+    sleep 40
   done
 fi
 chown -R chronos:chronos /home/chronos/user/containers
@@ -75,8 +81,7 @@ fi
 if ! lxc-ls -f | grep -E '\barch\b' > /dev/null; then
   echo Creating LXC Container
 
-  # Override PATH so lxc-create use Chromebrew sed instead of system
-  env PATH=/usr/local/bin:$PATH lxc-create \
+  lxc-create \
     -t download \
     -n arch \
     -- \
@@ -91,39 +96,7 @@ fi
 
 # Container startup
 if lxc-ls -f | grep -E '\barch\b' | grep STOPPED > /dev/null; then
-  cat <<EOF > /usr/local/var/lib/lxc/arch/config
-# Distribution configuration
-lxc.include = /usr/local/share/lxc/config/common.conf
-lxc.include = /usr/local/share/lxc/config/nesting.conf
-lxc.arch = x86_64
-
-lxc.cap.drop =
-
-# Container specific configuration
-lxc.rootfs.path = dir:/usr/local/var/lib/lxc/arch/rootfs
-lxc.uts.name = arch
-
-lxc.cgroup.devices.allow = a
-lxc.mount.auto = proc:rw sys:rw cgroup-full:mixed:force
-
-# Network configuration
-lxc.net.0.type = veth
-lxc.net.0.flags = up
-lxc.net.0.link = lxcbr0
-lxc.net.0.ipv4.address = 10.0.3.2/24
-lxc.net.0.ipv4.gateway = 10.0.3.1
-
-lxc.mount.entry=/home/chronos/user/Downloads/ home/cartolari/Downloads none bind,optional,create=dir
-lxc.mount.entry=/run/chrome/ opt/chrome none bind,optional,create=dir 0 0
-lxc.mount.entry=/home/chronos/user/.homesick/repos/chromebook-dotfiles/lxc/ usr/local/lxc-scripts none bind,optional,create=dir 0 0
-
-lxc.mount.entry = /dev/dri dev/dri none bind,optional,create=dir
-lxc.mount.entry = /dev/video0 dev/video0 none bind,optional,create=file
-lxc.mount.entry = /dev/kvm dev/kvm none bind,optional,create=file
-lxc.mount.entry = /dev/vboxdrv dev/vboxdrv none bind,optional,create=file
-lxc.mount.entry = /dev/net dev/net none bind,optional,create=dir
-
-lxc.mount.entry = /home/chronos/user/.homesick/repos/chromebook-dotfiles/lc/mount-loop-devices.service etc/systemd/system/mount-loop-devices.service none bind,optional,create=file
-EOF
+  rsync -azvpu "$DIR/arch/" /usr/local/var/lib/lxc/arch/
+  mkdir -p /var/lib/lxc/rootfs
   lxc-start -n arch -l debug --logfile /dev/stdout
 fi

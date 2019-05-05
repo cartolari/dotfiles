b58c851a670c69abc2fa3b99b5b700895e801e0d
cartolari <bruno.cartolari@gmail.com>
2019-05-05 03:47:21 -0300

    Add lots of devtools packages

--- home/.config/nixpkgs/config.nix
+++ home/.config/nixpkgs/config.nix
@@ -9,16 +9,21 @@
         (autojump.overrideAttrs (attrs: {
           meta.priority = 3;
         }))
+        aws
         bridge-utils
+        cloc
         curl
         dnsmasq
         docker
         docker-compose
         file
+        fontconfig
         fuse
         fzf
         git
+        git-lfs
         gnused
+        go
         gptfdisk
         grml-zsh-config
         homesick
@@ -29,23 +34,41 @@
         lxd
         man
         man-pages
+        ncurses5
         nodejs-11_x
+        open-sans
+        openbox
+        openconnect
         openssh
+        papirus-icon-theme
+        python
+        python3
         qemu
         rclone
+        rclone-browser
         readline70
+        ripgrep
+        rmlint
         (ruby_2_6.overrideAttrs (attrs: {
           meta.priority = 3;
         }))
+        s3cmd
+        shellcheck
         socat
+        sqlite
+        terraform
         tig
+        tigervnc
         tmux
         tree
+        unzip
+        vagrant
         (vimHugeX.overrideAttrs (attrs: {
           meta.priority = 3;
         }))
         wget
         xorg.xset
+        zip
         zsh
         zsh-autosuggestions
         zsh-syntax-highlighting

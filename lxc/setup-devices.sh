bbab0da848178bba602038e11804683418530b3c
cartolari <bruno.cartolari@gmail.com>
2019-05-08 00:52:59 -0300

    Remove LXC scripts
    * LXC containers stopped working for a reason unknown to me in
      CloudReady 74

--- lxc/setup-devices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[[ -b /dev/loop-control ]] || mknod -m 0660 /dev/loop-control b 10 237
-[[ -b /dev/fuse ]] || mknod -m 666 /dev/fuse c 10 229
-
-for i in $(seq 0 7); do
-  [[ -b /dev/loop$i ]] || mknod -m 0660 /dev/loop$i b 7 $i
-done

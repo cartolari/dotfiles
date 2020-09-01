e5e77a2ed0fdf26cfe80d291877f6ea7eb5471d7
cartolari <bruno.cartolari@gmail.com>
2020-09-01 05:29:19 +0000

    Add OpenVPN to the Chrome OS host

--- bootstrap/nix-kvm/host.sh
+++ bootstrap/nix-kvm/host.sh
@@ -51,6 +51,7 @@ echo Installing packages
 nix-env -f '<nixpkgs>' -iA bridge-utils
 nix-env -f '<nixpkgs>' -iA dnsmasq
 nix-env -f '<nixpkgs>' -iA gptfdisk
+nix-env -f '<nixpkgs>' -iA openvpn
 nix-env -f '<nixpkgs>' -iA qemu
 nix-env -f '<nixpkgs>' -iA socat
 nix-env -f '<nixpkgs>' -iA telnet

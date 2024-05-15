4aaefc85e1739f575930e412eebe87c074cb0c07
cartolari <bruno.cartolari@gmail.com>
2024-05-15 14:51:05 -0300

    Add Spin install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+if [[ -z "${SPIN:-}" ]]; then
+  echo Not a Spin installation, ignoring...
+
+  exit 0
+fi
+
+sudo apt install -qy \
+  autojump \
+  bat \
+  fzf \
+  homesick \
+  htop \
+  silversearcher-ag \
+  tig \
+  tmux \
+  tree \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting
+
+[[ -e /usr/bin/bat ]] || sudo ln -s /usr/bin/b{atc,}at
+
+homesick clone cartolari/dotfiles
+homesick link
